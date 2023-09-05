52e44c34dfbf6851b50e1b6937f333f63b02be2f
Ömer Kılıç <komer4542@gmail.com>
2023-09-06 00:59:34 +0300

    Updated.

--- MySQL Bağlantılı Veri oluşturma, Okuma, Güncellene ve Silme/index.php
+++ MySQL Bağlantılı Veri oluşturma, Okuma, Güncellene ve Silme/index.php
@@ -115,12 +115,12 @@
     </div>
     
     <div class="card-footer text-body-secondary">
-        <center>
-            <p>Design by <b>ÖMER KILIÇ </b> | &copy; 2023</p>
-        </center>
-    </div>
+    <center>
+        <p>Design by <a href="https://www.linkedin.com/in/%C3%B6mer-k%C4%B1l%C4%B1%C3%A7-5513b1252" target="_blank" style="text-decoration: none; color:black;"><b>ÖMER KILIÇ </b></a> | &copy; 2023</p>
+    </center>
+</div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 
